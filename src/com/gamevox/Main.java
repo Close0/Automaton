3d853ace6bc695e49a0782b0e1af455646c86f9f
Sean Huitger <sean@codebysean.com>
2014-06-02 23:56:47 -0400

    Fixed main like a noob.

--- src/com/gamevox/Main.java
+++ src/com/gamevox/Main.java
@@ -3,12 +3,11 @@ package com.gamevox;
 import com.gamevox.Account.AccountManager;
 
 public class Main {
-    public static void Main(String[] args) {
-        //Generate the accounts to be used by the bot
-        AccountManager.generateAccounts();
-
+    public static void main(String[] args) {
         try {
             //Testing ... Claim myself an account and list it out
+            //Generate the accounts to be used by the bot
+            AccountManager.generateAccounts();
             System.out.println(AccountManager.getRandomAccount().toString());
         }catch (Exception e) {
             System.out.println("Caught an Exception in Main: "+e.getMessage());
